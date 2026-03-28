65292b13035a7a984c1e0a05810357ec5352a925
Sanjay <sanjayjothilingam@gmail.com>
2026-03-28 17:04:29 +0530

    Update factorial.java

--- factorial.java
+++ factorial.java
@@ -9,5 +9,6 @@ class Factorial {
     public static void main(String[] args) {
         int num = 5;
         System.out.println("Factorial of " + num + " is: " + fact(num));
+        System.out.println("Webhook test");
     }
 }
