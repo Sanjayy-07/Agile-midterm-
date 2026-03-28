df58a1c02553809fadbabb0c6d29df40774f32c9
Sanjay <sanjayjothilingam@gmail.com>
2026-03-28 16:39:17 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,13 @@
+class Factorial {
+    static int fact(int n) {
+        if (n == 0)
+            return 1;
+        else
+            return n * fact(n - 1);
+    }
+
+    public static void main(String[] args) {
+        int num = 5;
+        System.out.println("Factorial of " + num + " is: " + fact(num));
+    }
+}
